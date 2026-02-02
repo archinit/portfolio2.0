1d1367f04b45bbfa3413a945f7b4246491685422
archinit <archit.7537@gmail.com>
2026-02-02 19:14:52 +0530

    landing modification

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
 export default function Home() {
-  return <div>
-    This is the portfolio project
-  </div>
+  return (
+    <main className="flex justify-center">
+      <div className="relative flex flex-col items-center min-h-screen min-w-4xl p-4">
+
+      <div className="absolute left-0 top-0 bottom-0 w-px border border-zinc-800 border-solid"></div>
+      <div className="absolute right-0 top-0 bottom-0 w-px border border-zinc-800 border-solid"></div>
+
+      <nav className="flex justify-between items-center">
+        <div className="flex">
+          <div>Home</div>
+          <div>Home</div>
+          <div>Home</div>
+        </div>
+
+        <div>
+          <button>dark</button>
+        </div>
+      </nav>
+         
+      </div>
+    </main>
+  )
 }
