cc448ff8792625d5da5b300794136ac94c8e10b3
archinit <archit.7537@gmail.com>
2026-02-04 19:00:02 +0530

    Navbar Updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,15 @@
 export const Navbar = () => {
-    return <div>
-        Navbar
-    </div>
+    return (
+            <nav className="container mx-auto flex justify-between">
+                <div className="flex gap-3"> 
+                    <a >Home</a >
+                    <a >Home</a >
+                    <a >Home</a >
+                </div>
+                <div className="flex">
+                    <button>dark</button>
+                </div>
+            </nav>
+
+    )
 }
