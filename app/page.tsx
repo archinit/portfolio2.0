dd279c489a48ae9bf13317e0c6f1c1f98c225a2c
archinit <archit.7537@gmail.com>
2026-02-04 19:00:37 +0530

    landing page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "@/components/Navbar";
+
 export default function Home() {
   return (
     <main className="flex justify-center">
@@ -6,17 +8,9 @@ export default function Home() {
       <div className="absolute left-0 top-0 bottom-0 w-px border border-zinc-800 border-solid"></div>
       <div className="absolute right-0 top-0 bottom-0 w-px border border-zinc-800 border-solid"></div>
 
-      <nav className="flex justify-between items-center">
-        <div className="flex">
-          <div>Home</div>
-          <div>Home</div>
-          <div>Home</div>
-        </div>
-
-        <div>
-          <button>dark</button>
-        </div>
-      </nav>
+      <div>
+      <Navbar />
+      </div>
          
       </div>
     </main>
