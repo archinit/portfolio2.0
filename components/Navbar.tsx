2bf437ae890e8ee7e929c7ec1334e445ff358156
Archit Banait <architbanait1@gmail.com>
2026-02-10 17:10:17 +0000

    Navbar updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,9 @@ export const Navbar = () => {
     return (
             <nav className="container mx-auto flex justify-between">
                 <div className="flex gap-3"> 
-                    <a >Home</a >
-                    <a >Projects</a >
-                    <a >About</a >
+                    <a>Home</a>
+                    <a>Projects</a>
+                    <a>About</a>
                 </div>
                 <div className="flex">
                     <button>dark</button>
