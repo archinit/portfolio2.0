ed981a3060380a66bc6728ac67ad447d35d9e1bb
archinit <architbanait1@gmail.com>
2026-02-27 18:10:40 +0000

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex justify-center">
       <div className="relative flex flex-col items-center min-h-screen min-w-4xl p-4">
 
-      <div className="absolute left-0 top-0 bottom-0 w-px border border-zinc-800 border-solid"></div>
+      <div className="absolute left-0 bottom-0 w-px border border-zinc-800 border-solid"></div>
       <div className="absolute right-0 top-0 bottom-0 w-px border border-zinc-800 border-solid"></div>
 
       <div>
