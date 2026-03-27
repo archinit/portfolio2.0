245d3e0fa7924afd61b88a8b76d8e5535c4d8dc8
archinit <architbanait1@gmail.com>
2026-03-27 23:44:41 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "@/components/Navbar";
 export default function Home() {
   return (
     <main className="flex justify-center">
-      <div className="relative flex-col items-center min-h-screen min-w-3xl p-4">
+      <div className="relative flex-col items-center min-h-screen min-w-4xl p-4">
 
       <div className="absolute left-0 bottom-0 w-px border border-zinc-800 border-solid"></div>
       <div className="absolute right-0 top-0 bottom-0 w-px border border-zinc-800 border-solid"></div>
